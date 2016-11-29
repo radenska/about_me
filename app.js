00cd3a09424466e39b56e4e0a46c7d75e41650fd
radenska <yana_radenska@yahoo.com>
2016-11-29 15:08:49 -0800

    added specific question content

--- app.js
+++ app.js
@@ -4,68 +4,70 @@ var prompt1 = 'Was Yana born in the United States?';
 var response1 = prompt(prompt1).toLowerCase();
 var answer1 = 'Yana was born in Bulgaria, not in the United States.';
 var defaultWrongResponse = 'I did not recognize your response.';
+var defaultCorrect = 'That is correct.\n';
+var defaultIncorrect = 'That is incorrect.\n';
 
 if (response1 === 'n' || response1 === 'no') {
-  alert('Good work!]\n' + answer1);
+  alert(defaultCorrect + answer1);
 }
 else if (response1 === 'y' || response1 === 'yes') {
-  alert('That is incorrect!\n' + answer1);
+  alert(defaultIncorrect + answer1);
 }
 else {
   alert(defaultWrongResponse);
 }
 
-var prompt2 = 'Was Yana born in the United States?';
+var prompt2 = 'Does Yana have any cats?';
 var response2 = prompt(prompt2).toLowerCase();
-var answer2 = 'Yana was born in Bulgaria, not in the United States.';
+var answer2 = 'Yana used to have three cats, but they\'re all dead.';
 
 if (response2 === 'n' || response2 === 'no') {
-  alert('Good work!]\n' + answer2);
+  alert(defaultCorrect + answer2);
 }
 else if (response2 === 'y' || response2 === 'yes') {
-  alert('That is incorrect!\n' + answer2);
+  alert(defaultIncorrect + answer2);
 }
 else {
   alert(defaultWrongResponse);
 }
 
-var prompt3 = 'Was Yana born in the United States?';
+var prompt3 = 'Is Yana\'s favorite color black?';
 var response3 = prompt(prompt1).toLowerCase();
-var answer3 = 'Yana was born in Bulgaria, not in the United States.';
+var answer3 = 'Yana\'s favorite color is black, and she \ndoesn\'t care that black is not technically a single color.';
 
 if (response3 === 'n' || response3 === 'no') {
-  alert('Good work!]\n' + answer3);
+  alert(defaultCorrect + answer3);
 }
 else if (response3 === 'y' || response3 === 'yes') {
-  alert('That is incorrect!\n' + answer3);
+  alert(defaultIncorrect + answer3);
 }
 else {
   alert(defaultWrongResponse);
 }
 
-var prompt4 = 'Was Yana born in the United States?'
+var prompt4 = 'Is Yana 42 years old?'
 var response4 = prompt(prompt1).toLowerCase();
-var answer4 = 'Yana was born in Bulgaria, not in the United States.';
+var answer4 = 'Yana is 37 years old.';
 
 if (response4 === 'n' || response4 === 'no') {
-  alert('Good work!]\n' + answer4);
+  alert(defaultCorrect + answer4);
 }
 else if (response4 === 'y' || response4 === 'yes') {
-  alert('That is incorrect!\n' + answer4);
+  alert(defaultIncorrect + answer4);
 }
 else {
   alert(defaultWrongResponse);
 }
 
-var prompt5 = 'Was Yana born in the United States?';
+var prompt5 = 'Did Yana want to be President of the US when she was younger?';
 var response5 = prompt(prompt1).toLowerCase();
-var answer5 = 'Yana was born in Bulgaria, not in the United States.';
+var answer5 = 'Yana did want to be President, but fortunately she became wiser as the years went on.';
 
 if (response5 === 'n' || response5 === 'no') {
-  alert('Good work!]\n' + answer5);
+  alert(defaultCorrect + answer5);
 }
 else if (response5 === 'y' || response5 === 'yes') {
-  alert('That is incorrect!\n' + answer5);
+  alert(defaultIncorrect + answer5);
 }
 else {
   alert(defaultWrongResponse);
