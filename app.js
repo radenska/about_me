a8eb323b0ae67ebf1e83cbd181a608fa8cf858fc
radenska <yana_radenska@yahoo.com>
2016-11-29 17:04:09 -0800

    added content to the html and the css file, added console log statements in the js file

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 'use strict';
 
+/*commenting this out while I test the rest of the site so I don't have to go through the questions while I do so
+
 var prompt1 = 'Was Yana born in the United States?';
 var response1 = prompt(prompt1).toLowerCase();
 var answer1 = 'Yana was born in Bulgaria, not in the United States.';
 var defaultWrongResponse = 'I did not recognize your response.';
 var defaultCorrect = 'That is correct.\n';
 var defaultIncorrect = 'That is incorrect.\n';
+var defaultGibberish = 'Their answer was gibberish.';
 
 if (response1 === 'n' || response1 === 'no') {
   alert(defaultCorrect + answer1);
+  console.log(prompt1 + ' They answered no. How did this person know I am an alien?');
 }
 else if (response1 === 'y' || response1 === 'yes') {
   alert(defaultIncorrect + answer1);
+  console.log(prompt1 + ' Ha! They think I was born in the US. I tricked them as I expected.');
 }
 else {
   alert(defaultWrongResponse);
+  console.log(prompt1 + ' ' + defaultGibberish);
 }
 
 var prompt2 = 'Does Yana have any cats?';
@@ -23,66 +29,78 @@ var answer2 = 'Yana used to have three cats, but they\'re all dead.';
 
 if (response2 === 'n' || response2 === 'no') {
   alert(defaultCorrect + answer2);
+  console.log(prompt2 + ' They answered no. Lucky guess, probably.');
 }
 else if (response2 === 'y' || response2 === 'yes') {
   alert(defaultIncorrect + answer2);
+  console.log(prompt2 + ' They answered yes. They must not be aware that all my cats died.');
 }
 else {
   alert(defaultWrongResponse);
+  console.log(prompt2 + ' ' + defaultGibberish);
 }
 
 var prompt3 = 'Is Yana\'s favorite color black?';
-var response3 = prompt(prompt1).toLowerCase();
+var response3 = prompt(prompt3).toLowerCase();
 var answer3 = 'Yana\'s favorite color is black, and she \ndoesn\'t care that black is not technically a single color.';
 
 if (response3 === 'n' || response3 === 'no') {
   alert(defaultCorrect + answer3);
+  console.log(prompt3 + ' They answered no. They clearly know very little about me.');
 }
 else if (response3 === 'y' || response3 === 'yes') {
   alert(defaultIncorrect + answer3);
+  console.log(prompt3 + ' They answered yes. They know me well.');
 }
 else {
   alert(defaultWrongResponse);
+  console.log(prompt3 + ' ' + defaultGibberish);
 }
 
 var prompt4 = 'Is Yana 42 years old?'
-var response4 = prompt(prompt1).toLowerCase();
+var response4 = prompt(prompt4).toLowerCase();
 var answer4 = 'Yana is 37 years old.';
 
 if (response4 === 'n' || response4 === 'no') {
   alert(defaultCorrect + answer4);
+  console.log(prompt4 + ' They answered no. Good, now I don\'t have to kill them in their sleep for thinking I\' m for thinking I am fiver years older than I am.');
 }
 else if (response4 === 'y' || response4 === 'yes') {
   alert(defaultIncorrect + answer4);
+  console.log(prompt4 + ' They answered yes. Since they think I am five years older than I am, I will have to kills them in their sleep. ');
 }
 else {
   alert(defaultWrongResponse);
+  console.log(prompt4 + ' ' + defaultGibberish);
 }
 
 var prompt5 = 'Did Yana want to be President of the US when she was younger?';
-var response5 = prompt(prompt1).toLowerCase();
+var response5 = prompt(prompt5).toLowerCase();
 var answer5 = 'Yana did want to be President, but fortunately she became wiser as the years went on.';
 
 if (response5 === 'n' || response5 === 'no') {
   alert(defaultCorrect + answer5);
+  console.log(prompt5 + ' They answered no. They probably didn\'t want to believe I could be that stupid. ');
 }
 else if (response5 === 'y' || response5 === 'yes') {
   alert(defaultIncorrect + answer5);
+  console.log(prompt5 + ' They answered yes. How did they know I wanted to be President when I was young?');
 }
 else {
   alert(defaultWrongResponse);
+  console.log(prompt5 + ' ' + defaultGibberish);
 }
 
-document.write('Here is a summary of the game, the questions asked, and your answers: </br>');
-document.write(prompt1 + 'You answered ' + question1);
+document.write('Here is a summary of the game, the questions asked, and your answers: </br></br>');
+document.write(prompt1 + ' You answered \'' + response1 + '\'.');
 document.write('</br>' + answer1 + '</br></br>');
-document.write('test test');
-document.write(prompt5 + 'You answered ' + question2);
+document.write(prompt2 + ' You answered \'' + response2 + '\'.');
 document.write('</br>' + answer2 + '</br></br>');
-document.write(prompt3 + 'You answered ' + question3);
+document.write(prompt3 + ' You answered \'' + response3 + '\'.');
 document.write('</br>' + answer3 + '</br></br>');
-document.write('test test');
-document.write(prompt4 + 'You answered ' + question4);
+document.write(prompt4 + ' You answered \'' + response4 + '\'.');
 document.write('</br>' + answer4 + '</br></br>');
-document.write(prompt5 + 'You answered ' + question5);
+document.write(prompt5 + ' You answered \'' + response5 + '\'.');
 document.write('</br>' + answer5 + '</br></br>');
+
+*/
